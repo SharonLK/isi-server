e390903ce4ad115002d36891857e0fbfb778320d
Sharon Levenzon Kuninin <sharonlevenzonkuninin@gmail.com>
2018-06-03 09:38:05 -0400

    Basic implementation of faas-cli List command
    In addition add Log4j dependency.

--- src/main/java/faas/FaasCLI.java
+++ src/main/java/faas/FaasCLI.java
@@ -1,5 +1,30 @@
 package faas;
 
-public class FaasCLI {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
+public class FaasCLI {
+    private final static Logger logger = Logger.getLogger(FaasCLI.class.getName());
+
+    /**
+     * @return
+     */
+    public static List<String> functions() throws IOException, InterruptedException {
+        Process exec = Runtime.getRuntime().exec("faas-cli list");
+        exec.waitFor();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(exec.getInputStream()));
+
+        List<String> functions = new ArrayList<String>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            functions.add(line);
+        }
+
+        return functions;
+    }
 }
