fbdf76ed3e9f8b4fce5c8885b01c1b109443e2ac
Sharon Levenzon Kuninin <sharonlevenzonkuninin@gmail.com>
2018-06-06 11:41:09 +0300

    Add support for retrieval of sent files

--- src/main/java/server/Communicator.kt
+++ src/main/java/server/Communicator.kt
@@ -2,6 +2,8 @@ package server
 
 import com.sun.net.httpserver.HttpServer
 import org.apache.commons.io.IOUtils
+import java.io.BufferedOutputStream
+import java.io.FileOutputStream
 import java.net.InetSocketAddress
 
 fun main(args: Array<String>) {
@@ -22,6 +24,26 @@ class Communicator {
             val body = exchange.responseBody
             body.write(data.toByteArray())
             body.close()
+        })
+        server.createContext("/post", { exchange ->
+            println("Receiving message")
+
+            val bos = BufferedOutputStream(FileOutputStream("/home/sharon/a.txt"))
+            val buff = ByteArray(2048)
+            val requestBody = exchange.requestBody
+            var c = requestBody.read(buff)
+
+            while (c > 0) {
+                bos.write(buff, 0, c)
+                c = requestBody.read(buff)
+            }
+
+            bos.close()
+            requestBody.close()
+
+            println("Message received")
+
+            exchange.sendResponseHeaders(200, 0)
         })
 
         server.executor = null
