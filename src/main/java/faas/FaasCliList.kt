bfb99db06867757ac113a1c836b216e3e307b7eb
Sharon Levenzon Kuninin <sharonlevenzonkuninin@gmail.com>
2018-06-07 08:05:57 +0300

    Some refactoring for the wrapper

--- src/main/java/faas/FaasCliList.kt
+++
@@ -1,4 +0,0 @@
-package faas
-
-class FaasCliList (val functionName : String, val invocations : Int, val replicas : Int) {
-}
--- src/main/java/faas/FaasWrapper.kt
+++ src/main/java/faas/FaasWrapper.kt
@@ -1,11 +1,12 @@
 /**
- * require access to sudo.
+ * Require access to sudo.
  * for easy access, you can permit use of sudo functions without password for the user.
  *
  * commands to run:
  * sudo adduser ThisUser sudo or in earlier versions of linux - sudo adduser ThisUser admins
  * then run the command sudo visudo and in the editors change the line thats start with $sudo with:
- * $sudo    ALL=NOPASSWD: ALL
+ *
+ *  $sudo    ALL=NOPASSWD: ALL
  */
 
 package faas
@@ -13,78 +14,73 @@ package faas
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun main(args: Array<String>){
-    var faasList = list()
+fun main(args: Array<String>) {
+    val faasList = list()
     faasList.forEach {
         print("${it.functionName}\t${it.invocations}\t${it.replicas}\n")
     }
 }
 
-fun list(): List<FaasCliList>{
-    var listString = runCommand("faas-cli list")
-    var result : MutableList<FaasCliList> = mutableListOf()
+data class FaasCliList (val functionName : String, val invocations : Int, val replicas : Int)
+
+fun list(): List<FaasCliList> {
+    val listString = runCommand("faas-cli list")
+    val result: MutableList<FaasCliList> = ArrayList()
     var lines = listString.lines()
-    lines = lines.subList(1, lines.size-1)
-    lines.forEach{
-        var parm = it.split("\\s".toRegex())
-        var actualString : MutableList<String> = mutableListOf()
-        for(i in 0 until parm.size-1){
-            if (parm[i] != " " && parm[i] != "") {
-                actualString.add(parm[i])
-            }
-        }
-        result.add(FaasCliList(actualString[0], Integer.parseInt(actualString[1]), Integer.parseInt(actualString[2])))
+    lines = lines.subList(1, lines.size)
+
+    lines.forEach {
+        val split = it.split("\\s+".toRegex())
+        result.add(FaasCliList(split[0], Integer.parseInt(split[1]), Integer.parseInt(split[2])))
     }
 
     return result
 }
 
-fun remove(functionName : String){
+fun remove(functionName: String) {
     println(runCommand("faas-cli remove $functionName`"))
 
 }
 
-fun build(image : String, lang : String = "python3", name : String) {
+fun build(image: String, lang: String = "python3", name: String) {
     println(runCommand("sudo faas-cli build --image $image --lang $lang --name $name"))
 }
 
-fun deploy(image : String, lang : String = "python3", name : String) {
+fun deploy(image: String, lang: String = "python3", name: String) {
     println(runCommand("sudo faas-cli deploy --image $image --lang $lang --name $name"))
 }
 
-fun buildAndDeploy(image : String, lang : String = "python3", name : String) {
+fun buildAndDeploy(image: String, lang: String = "python3", name: String) {
     build(image, lang, name)
     deploy(image, lang, name)
 }
 
 
-fun runCommand(command : String) : String {
+fun runCommand(command: String): String {
     val rt = Runtime.getRuntime()
     val proc = rt.exec(command)
 
     val stdInput = BufferedReader(InputStreamReader(proc.inputStream))
-
     val stdError = BufferedReader(InputStreamReader(proc.errorStream))
 
     println("\nThe command is: $command\n")
-// read the output from the command
-    //println("\nHere is the standard output of the command:\n")
-    var result = ""
-    var s: String?
-    s = stdInput.readLine()
-    while (s != null) {
-        result += s+"\n"
-        //println(s)
-        s = stdInput.readLine()
-    }
-    result = result.substring(0, result.length-2)
 
-// read any errors from the attempted command
-    //println("\nHere is the standard error of the command (if any):\n")
-    s = stdError.readLine()
-    while (s != null) {
-        println(s)
-        s = stdError.readLine()
+    // Read the output from the command
+    var result = ""
+    var line: String?
+    line = stdInput.readLine()
+    while (line != null) {
+        result += "$line\n"
+        line = stdInput.readLine()
+    }
+
+    result = result.substring(0, result.length - 2)
+
+    // Read any errors from the attempted command
+    line = stdError.readLine()
+    while (line != null) {
+        println(line)
+        line = stdError.readLine()
     }
 
     return result
