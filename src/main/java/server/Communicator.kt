5e822a517c26582601f76abfbb79f5159f0fce03
Sharon Levenzon Kuninin <sharonlevenzonkuninin@gmail.com>
2018-06-07 08:26:28 +0300

    Add documentation & use kotlin extension functions to read from streams

--- src/main/java/server/Communicator.kt
+++ src/main/java/server/Communicator.kt
@@ -24,12 +24,14 @@ class Communicator {
     private val mainDirPath: String
 
     init {
+        // Parse the main directory path from the config.json file
         val parser = JSONParser()
         val config = parser.parse(IOUtils.toString(this.javaClass.classLoader.getResource("config.json"))) as JSONObject
         mainDirPath = config["path"] as String
     }
 
     fun start() {
+        // Open a local server on port 9091 and add two basic handlers
         val server = HttpServer.create(InetSocketAddress(9091), 0)
         server.createContext("/", this::handleSendData)
         server.createContext("/post", this::handleReceiveZip)
@@ -38,6 +40,9 @@ class Communicator {
         server.start()
     }
 
+    /**
+     * Handlers initial GET requests from the client and sends them currently deployed functions
+     */
     private fun handleSendData(exchange: HttpExchange) {
         exchange.sendResponseHeaders(200, data.length.toLong())
         val body = exchange.responseBody
@@ -45,51 +50,41 @@ class Communicator {
         body.close()
     }
 
+    /**
+     * Handlers POST requests from the client for deployment of new functions
+     */
     private fun handleReceiveZip(exchange: HttpExchange) {
         println("Receiving message")
 
+        // Parse headers
         val name = exchange.requestHeaders.getFirst("func_name")
         val replicas = exchange.requestHeaders.getFirst("replicas")
 
+        // Make directories needed for the new function
         File("$mainDirPath/$name").mkdirs()
 
+        // Read the send ZIP file into a local file on this server that will be stored in '$mainDirPath/a.zip'
         val bos = BufferedOutputStream(FileOutputStream("$mainDirPath/a.zip"))
-        val buff = ByteArray(2048)
-        val requestBody = exchange.requestBody
-        var c = requestBody.read(buff)
-
-        while (c > 0) {
-            bos.write(buff, 0, c)
-            c = requestBody.read(buff)
-        }
-
+        bos.write(exchange.requestBody.readBytes())
         bos.close()
-        requestBody.close()
 
         println("Message received")
 
+        // Send response to the client that the file has been received successfully
         exchange.sendResponseHeaders(200, 0)
 
+        // Unzip the file into a new folder that was created especially for this new function
         val zis = ZipInputStream(FileInputStream("$mainDirPath/a.zip"))
         var zipEntry = zis.nextEntry
 
         while (zipEntry != null) {
-            val fileName = zipEntry.name
-            val newFile = File("$mainDirPath/$name/$fileName")
-            val fos = FileOutputStream(newFile)
-            var len = zis.read(buff)
-
-            while (len > 0) {
-                fos.write(buff, 0, len)
-
-                len = zis.read(buff)
-            }
-
+            val fos = FileOutputStream("$mainDirPath/$name/${zipEntry.name}")
+            fos.write(zis.readBytes())
             fos.close()
+
             zipEntry = zis.nextEntry
         }
 
-        zis.closeEntry()
         zis.close()
     }
 }
